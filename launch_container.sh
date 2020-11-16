343e2f0887928893f24e376b5082a50482353e1b
butellyn <ellynrbutler@gmail.com>
2020-11-16 16:12:30 -0500

    construct group template and composite warps

--- launch_container.sh
+++ launch_container.sh
@@ -1,11 +1,15 @@
 docker run --rm -ti --entrypoint=/bin/bash \
   -v /Users/butellyn/Documents/ExtraLong/data/singleSubjectTemplates/antssst/sub-100079/ses-motive1/sub-100079_ses-motive1_desc-preproc_T1w0Warp.nii.gz:/data/input/sub-100079_ses-motive1_desc-preproc_T1w0Warp.nii.gz \
+  -v /Users/butellyn/Documents/ExtraLong/data/singleSubjectTemplates/antssst/sub-100079/ses-motive1/sub-100079_ses-motive1_desc-preproc_T1w0Affine.txt:/data/input/sub-100079_ses-motive1_desc-preproc_T1w0Affine.txt \
   -v /Users/butellyn/Documents/ExtraLong/data/singleSubjectTemplates/antssst/sub-100079/ses-PNC2/sub-100079_ses-PNC2_desc-preproc_T1w1Warp.nii.gz:/data/input/sub-100079_ses-PNC2_desc-preproc_T1w1Warp.nii.gz \
+  -v /Users/butellyn/Documents/ExtraLong/data/singleSubjectTemplates/antssst/sub-100079/ses-PNC2/sub-100079_ses-PNC2_desc-preproc_T1w1Affine.txt:/data/input/sub-100079_ses-PNC2_desc-preproc_T1w1Affine.txt \
   -v /Users/butellyn/Documents/ExtraLong/data/freesurferCrossSectional/fmriprep/sub-100079/ses-motive1/anat/sub-100079_ses-motive1_desc-aseg_dseg.nii.gz:/data/input/sub-100079_ses-motive1_desc-aseg_dseg.nii.gz \
   -v /Users/butellyn/Documents/ExtraLong/data/freesurferCrossSectional/fmriprep/sub-100079/ses-PNC2/anat/sub-100079_ses-PNC2_desc-aseg_dseg.nii.gz:/data/input/sub-100079_ses-PNC2_desc-aseg_dseg.nii.gz \
   -v /Users/butellyn/Documents/ExtraLong/data/singleSubjectTemplates/antssst/sub-100079/sub-100079_template0.nii.gz:/data/input/sub-100079_template0.nii.gz \
   -v /Users/butellyn/Documents/ExtraLong/data/singleSubjectTemplates/antssst/sub-87346/ses-10597/sub-87346_ses-10597_desc-preproc_T1w0Warp.nii.gz:/data/input/sub-87346_ses-10597_desc-preproc_T1w0Warp.nii.gz \
+  -v /Users/butellyn/Documents/ExtraLong/data/singleSubjectTemplates/antssst/sub-87346/ses-10597/sub-87346_ses-10597_desc-preproc_T1w0Affine.txt:/data/input/sub-87346_ses-10597_desc-preproc_T1w0Affine.txt \
   -v /Users/butellyn/Documents/ExtraLong/data/singleSubjectTemplates/antssst/sub-87346/ses-PNC1/sub-87346_ses-PNC1_desc-preproc_T1w1Warp.nii.gz:/data/input/sub-87346_ses-PNC1_desc-preproc_T1w1Warp.nii.gz \
+  -v /Users/butellyn/Documents/ExtraLong/data/singleSubjectTemplates/antssst/sub-87346/ses-PNC1/sub-87346_ses-PNC1_desc-preproc_T1w1Affine.txt:/data/input/sub-87346_ses-PNC1_desc-preproc_T1w1Affine.txt \
   -v /Users/butellyn/Documents/ExtraLong/data/freesurferCrossSectional/fmriprep/sub-87346/ses-10597/anat/sub-87346_ses-10597_desc-aseg_dseg.nii.gz:/data/input/sub-87346_ses-10597_desc-aseg_dseg.nii.gz \
   -v /Users/butellyn/Documents/ExtraLong/data/freesurferCrossSectional/fmriprep/sub-87346/ses-PNC1/anat/sub-87346_ses-PNC1_desc-aseg_dseg.nii.gz:/data/input/sub-87346_ses-PNC1_desc-aseg_dseg.nii.gz \
   -v /Users/butellyn/Documents/ExtraLong/data/singleSubjectTemplates/antssst/sub-87346/sub-87346_template0.nii.gz:/data/input/sub-87346_template0.nii.gz \
--- run.sh
+++ run.sh
@@ -13,7 +13,34 @@ OutDir=/data/output # Bind /project/ExtraLong/data/groupTemplates
 # ^ The type of tissue will need to be determined for each label in this image (i.e., GM, WM, CSF)
 # 2) ?
 
-# Create GM, WM and CSF images for each segmentation
+###### 1.) Create tissue classification images for each segmentation
+#https://github.com/ANTsX/ANTs/blob/master/Scripts/antsCookTemplatePriors.sh - old way
 python masks.py
 
-#https://github.com/ANTsX/ANTs/blob/master/Scripts/antsCookTemplatePriors.sh - old way
+###### 2.) Create a group template from the SSTs
+ssts=`find ${InDir} -name "*template*"`
+for image in ${ssts}; do echo "${image}" >> ${OutDir}/tmp_subjlist.csv ; done
+antsMultivariateTemplateConstruction.sh -d 3 -o "${OutDir}/ExtraLongTemplate" -n 0 -c 2 -j 2 ${OutDir}/tmp_subjlist.csv
+#Change name of group template to ExtraLongTemplate.nii.gz
+
+###### 3.) Concatenate the transforms from T1w-space to group template space
+t1wToSSTwarps=`find ${InDir} -name "*Warp.nii.gz"`
+for warp in ${t1wToSSTwarps}; do
+  bblid=`echo ${warp} | cut -d "_" -f 1 | cut -d "-" -f 2`
+  sesid=`echo ${warp} | cut -d "_" -f 2 | cut -d "-" -f 2`
+  antsApplyTransforms \
+   -d 3 \
+   -e 0 \
+   -o [${OutDir}/sub-${bblid}_ses-${sesid}_NormalizedtoExtraLongTemplateCompositeWarp.nii.gz, 1] \
+   -r ${OutDir}/ExtraLongTemplate.nii.gz \ #Output from line 23: group template
+   -t ${OutDir}/sub-${bblid}_SSTNormalizedtoExtraLongTemplate1Warp.nii.gz \ #Output from line 23: SST warped to group template
+   -t ${OutDir}/sub-${bblid}_SSTNormalizedtoExtraLongTemplate0GenericAffine.mat \ #Output from line 23: affine for SST to group
+   -t ${InDir}/${warp} \ #Output from antssst
+   -t ${InDir}/sub-${bblid}_ses-${sesid}_T1w*Affine.mat #Output from antssst: NEEDS TO BE BOUND
+done
+
+###### 4.) Warp the tissue classification images in T1w-space to the group template space
+
+
+###### 5.) Average all of the tissue classication images in the group template space
+###### to create tissue class priors
