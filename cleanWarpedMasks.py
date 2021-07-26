8c80fc69fc08bc3c7abb7813c8520cd7cb26b85e
Katja Zoner <katja.zoner@pennmedicine.upenn.edu>
2021-07-26 10:20:04 -0400

    Improved how warped masks are gathered using glob. Outputs directly to masks subdir.

--- cleanWarpedMasks.py
+++ cleanWarpedMasks.py
@@ -4,20 +4,24 @@
 ### Ellyn Butler
 ### February 3, 2021
 
-import nibabel as nib
+import os
+import glob
 import numpy as np
 import pandas as pd
-import os
+import nibabel as nib
 from copy import deepcopy
 
-# List the available aseg images
-files = os.listdir('/data/output/')
-warpedmasks = [file for file in files if '_mask_NormalizedtoExtraLongTemplate.nii.gz' in file]
+# Path to output dir
+maskDir= '/data/output/masks/'
 
-for wmask in warpedmasks:
-    mask_img = nib.load('/data/output/'+wmask)
+# Get list of all warped tissue masks
+warped_masks = glob.glob(maskDir + '*WarpedTo*Template.nii.gz')
+
+# Clean each mask by converting values <= 0.2 to 0.0
+for mask in warped_masks:
+    mask_img = nib.load(mask)
     mask_data = mask_img.get_fdata()
     #mask_data[mask_data > .5] = 1.0
     mask_data[mask_data <= .2] = 0.0
     bin_mask_img = nib.Nifti1Image(mask_data, affine=mask_img.affine)
-    bin_mask_img.to_filename('/data/output/'+wmask.replace('mask', 'clean'))
+    bin_mask_img.to_filename(mask.replace('mask', 'clean'))
