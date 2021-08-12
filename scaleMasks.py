71e6338a19b771c1da98779870c7491f69190e5c
Katja Zoner <katja.zoner@pennmedicine.upenn.edu>
2021-08-12 10:56:39 -0400

    Fixed filename for finding mask file. Added projectName to tissue priors output filenames.

--- scaleMasks.py
+++ scaleMasks.py
@@ -5,14 +5,11 @@
 ### Ellyn Butler
 ### November 17, 2020 - February 4, 2021
 
-
-import os
+import re
 import glob
 import numpy as np
 import pandas as pd
 import nibabel as nib
-from copy import deepcopy
-from numpy import inf
 
 # Path to masks dir
 maskDir= '/data/output/masks/'
@@ -22,26 +19,30 @@ tissues_dict = {
     'GMCortical':[], 'GMDeep':[], 'WMCortical':[]}
 
 # Declare an empty array in shape of tissue mask images.
-maskFile = glob.glob(maskDir + "*" + tissue + "-clean_WarpedTo*Template.nii.gz")[0]
+maskFile = glob.glob(maskDir + "*-clean_WarpedTo*Template.nii.gz")[0]
 img = nib.load(maskFile)
 img_array = img.get_fdata()
 sum_all_tissues = np.zeros(img_array.shape)
 
+# Get project name from maskFile name
+pattern = "WarpedTo(.*?)Template"
+projectName = re.search(pattern, maskFile).group(1)
+
 # For each tissue type, sum mask arrays and story in dict.
 for tissue in tissues_dict.keys():
-    
+
     # Get all cleaned, normalized masks of current tissue type
     masks = glob.glob(maskDir + "*" + tissue + "-clean_WarpedTo*Template.nii.gz")
-        
+    
     mask_arrays = []
     for mask in masks:
         img = nib.load(mask)
         img_array = img.get_fdata()
         mask_arrays.append(img_array)
-    
+
     # Sum values across all masks of given tissue type.
     tissues_dict[tissue] = np.sum(mask_arrays, axis=0)
-
+    
     # Add sum for current tissue type to overall tissues sum
     sum_all_tissues = sum_all_tissues + tissues_dict[tissue]
 
