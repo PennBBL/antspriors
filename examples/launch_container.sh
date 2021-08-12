8399ab57c069866cba001127b43c76d0e88c064a
Katja Zoner <katja.zoner@pennmedicine.upenn.edu>
2021-08-12 12:31:57 -0400

    Updated launch script for antspriors v0.1.0 on PMACS

--- examples/launch_container.sh
+++ examples/launch_container.sh
@@ -1,19 +1,46 @@
-docker run --rm -ti --entrypoint=/bin/bash -e projectName="ExtraLong" -e NumSSTs=8 -e atlases="nowhitematter" \
-  -v /Users/butellyn/Documents/ExtraLong/data/singleSubjectTemplates/antssst5/sub-100079:/data/input/antssst/sub-100079 \
-  -v /Users/butellyn/Documents/ExtraLong/data/freesurferCrossSectional/fmriprep/sub-100079:/data/input/fmriprep/sub-100079 \
-  -v /Users/butellyn/Documents/ExtraLong/data/singleSubjectTemplates/antssst5/sub-113054:/data/input/antssst/sub-113054 \
-  -v /Users/butellyn/Documents/ExtraLong/data/freesurferCrossSectional/fmriprep/sub-113054:/data/input/fmriprep/sub-113054 \
-  -v /Users/butellyn/Documents/ExtraLong/data/mindboggle/dataverse_files:/data/input/dataverse_files \
-  -v /Users/butellyn/Documents/ExtraLong/data/groupTemplates/antspriors:/data/output \
-  -v /Users/butellyn/Documents/ExtraLong/data/mindboggleVsBrainCOLOR_Atlases:/data/input/mindboggleVsBrainCOLOR_Atlases \
-  pennbbl/antspriors:0.0.36
+#!/bin/bash
 
-SINGULARITYENV_projectName=ExtraLong SINGULARITYENV_NumSSTs=8 SINGULARITYENV_NumSSTs=nowhitematter singularity run --writable-tmpfs --cleanenv \
-  -B /project/ExtraLong/data/singleSubjectTemplates/antssst5/sub-100079:/data/input/antssst/sub-100079 \
-  -B /project/ExtraLong/data/freesurferCrossSectional/fmriprep/sub-100079:/data/input/fmriprep/sub-100079 \
-  -B /project/ExtraLong/data/singleSubjectTemplates/antssst5/sub-113054:/data/input/antssst/sub-113054 \
-  -B /project/ExtraLong/data/freesurferCrossSectional/fmriprep/sub-113054:/data/input/fmriprep/sub-113054 \
-  -B /project/ExtraLong/data/mindboggle/dataverse_files:/data/input/dataverse_files \
-  -B /project/ExtraLong/data/groupTemplates/antspriors:/data/output \
-  -B /project/ExtraLong/data/mindboggleVsBrainCOLOR_Atlases:/data/input/mindboggleVsBrainCOLOR_Atlases \
-  /project/ExtraLong/images/antspriors_0.0.36.sif
+export LOGS_DIR=/home/kzoner/logs/ants/antspriors
+mkdir -p ${LOGS_DIR}
+
+jsDir=~/ants_pipelines/scripts/jobscripts/antspriors
+mkdir -p ${jsDir}
+
+fmriprep_dir=/project/ExtraLong/data/freesurferCrossSectional/fmriprep
+antssst_dir=~/ants_pipelines/data/singleSubjectTemplates/antssst+jlf-0.1.0
+atlas_dir=~/ants_pipelines/data/mindboggleVsBrainCOLOR_Atlases
+
+out_dir=~/ants_pipelines/data/groupTemplates/antspriors-0.1.0
+mkdir -p ${out_dir}
+
+jobscript=${jsDir}/antspriors+jlf-0.1.0.sh
+
+cat <<- JOBSCRIPT > ${jobscript}
+  #!/bin/bash 
+  singularity run --writable-tmpfs --cleanenv --containall \\
+    -B ${fmriprep_dir}/sub-91404:/data/input/fmriprep/sub-91404 \\
+    -B ${fmriprep_dir}/sub-85392:/data/input/fmriprep/sub-85392 \\
+    -B ${fmriprep_dir}/sub-93811:/data/input/fmriprep/sub-93811 \\
+    -B ${fmriprep_dir}/sub-100079:/data/input/fmriprep/sub-100079 \\
+    -B ${fmriprep_dir}/sub-107903:/data/input/fmriprep/sub-107903 \\
+    -B ${fmriprep_dir}/sub-108315:/data/input/fmriprep/sub-108315 \\
+    -B ${fmriprep_dir}/sub-114990:/data/input/fmriprep/sub-114990 \\
+    -B ${fmriprep_dir}/sub-116147:/data/input/fmriprep/sub-116147 \\
+    -B ${antssst_dir}/sub-85392:/data/input/antssst/sub-85392 \\
+    -B ${antssst_dir}/sub-91404:/data/input/antssst/sub-91404 \\
+    -B ${antssst_dir}/sub-93811:/data/input/antssst/sub-93811 \\
+    -B ${antssst_dir}/sub-100079:/data/input/antssst/sub-100079 \\
+    -B ${antssst_dir}/sub-107903:/data/input/antssst/sub-107903 \\
+    -B ${antssst_dir}/sub-108315:/data/input/antssst/sub-108315 \\
+    -B ${antssst_dir}/sub-114990:/data/input/antssst/sub-114990 \\
+    -B ${antssst_dir}/sub-116147:/data/input/antssst/sub-116147 \\      
+    -B ${atlas_dir}:/data/input/atlases \\
+    -B ${out_dir}:/data/output \\
+    ~/ants_pipelines/images/antspriors_0.1.0.sif --project ExtraLong --seed 1 --jlf
+
+JOBSCRIPT
+
+chmod +x ${jobscript}
+bsub -e $LOGS_DIR/antspriors+jlf-0.1.0.e -o $LOGS_DIR/antspriors+jlf-0.1.0.o ${jobscript}
+
+done
