e92b565b0a5a66da6940ee8ba4d6916bc911b858
katjz <katjazoner@gmail.com>
2022-03-02 15:35:09 -0500

    Add line to bind atlas dir

--- examples/run_singularity.sh
+++ examples/run_singularity.sh
@@ -12,6 +12,7 @@ mkdir -p ${jsDir}
 data="/project/ExtraLong/data/datafreeze-2021"
 fmriprep_dir=${data}/fmriprep
 antslong_dir=${data}/ANTsLongitudinal
+atlas_dir=${data}/../mindboggleVsBrainCOLOR_Atlases
 
 gt_subs_csv=${data}/QC/group_template_subjects.csv
 subList=$(cat ${gt_subs_csv} | tail -n +2)
@@ -35,6 +36,7 @@ cat <<- JOBSCRIPT > ${jobscript}
 	
 	singularity run --writable-tmpfs --cleanenv \\
 		${fmriprep_bindings}
+		-B ${atlas_dir}:/data/input/atlases \\
 		-B ${antslong_dir}:/data/output \\
 		/project/ExtraLong/images/antspriors_0.1.0.sif --project ExtraLong --seed 1 --jlf
 
